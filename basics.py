bd26b00e151714102f34e0396076dccdc9f6ddc3
gari426-beep <gaurikaur426@gmail.com>
2026-03-02 21:31:06 +0530

    Add basic Python practice script

---
+++ basics.py
@@ -0,0 +1,12 @@
+# Basic Python Practice
+
+name = "Gauri"
+age = 18 
+
+print("Hello my name is " , name)
+print("I am", age ,"years old)
+
+# Simple Calculation
+a = 5
+b = 3
+print("sum is:", a+b)
